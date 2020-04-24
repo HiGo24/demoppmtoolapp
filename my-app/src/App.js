7deb3ee8e0e407aece353e76d4cec38b191b7294
HiGo <govindsingh2081@gmail.com>
2020-04-25 00:09:09 +0530

    Error handling Validation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React,{Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Components/Dashboard';
 import Header from './Components/Layout/Header';
 import AddProject from './Components/projects/AddProject';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router , Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <Router>
-    <div>
-        <Header/>
-    </div>
-    <Route path="/addProject" component={AddProject}/>
-    <Route path="/dashboard" component={Dashboard}/>
-    
-    </Router>
-  );
+import {Provider} from "react-redux";
+import store from "./store";
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addProject" component={AddProject} />
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- my-app/src/acttions/ProjectActions.js
+++ my-app/src/acttions/ProjectActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GET_ERRORS } from "./type";
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("http://192.168.2.108:8000/api/project", project);
     history.push("/dashboard");
   } catch (error) {
     dispatch({
@@ -11,3 +11,6 @@ export const createProject = (project, history) => async dispatch => {
     });
   }
 };
+
+
+//http://192.168.2.108:8000/api/project
